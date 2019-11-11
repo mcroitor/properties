4617cbd3dd8bc497ab478b14d9989e6d94ed64a8
Михаил Кройтор <m_croitor@hotmail.com>
2019-11-11 11:11:57 +0200

    all documentation is moved to header file

--- properties.cpp
+++ properties.cpp
@@ -37,21 +37,11 @@ std::string trim(std::string str) {
 
 namespace mc {
 
-    /**
-     * default constructor. associated implicitly with file "default.properties"
-     */
     properties::properties() : _property_filename("default.properties") {
     }
 
-    /**
-     * destructor
-     */
     properties::~properties() = default;
 
-    /**
-     * load properties from file <b>filename</b>
-     * @param filename
-     */
     void properties::load_properties(const std::string& filename) {
         _property_filename = filename;
         std::ifstream fin(_property_filename);
@@ -78,55 +68,26 @@ namespace mc {
         fin.close();
     }
 
-    /**
-     * check if property exists. return false if property <b>key</b>
-     * does not exist.
-     * @param key
-     * @return 
-     */
     bool properties::has_property(const std::string& key) const {
         return _properties.count(key) == 1;
     }
 
-    /**
-     * return value of property <b>key</b>.
-     * is the same as <b>getProperty</b>.
-     * @param key
-     * @return 
-     */
     const std::string& properties::operator[](const std::string& key) const {
         return _properties.at(key);
     }
 
-    /**
-     * return value of property <b>key</b>.
-     * is the same as <b>operator []</b>.
-     * @param key
-     * @return 
-     */
     const std::string& properties::get_property(const std::string& key) const {
         return _properties.at(key);
     }
 
-    /**
-     * returns name of associated property file
-     * @return 
-     */
     const std::string& properties::filename() const {
         return _property_filename;
     }
 
-    /**
-     * returns all properties as map
-     * @return 
-     */
     const std::map<std::string, std::string>& properties::all_properties() const {
         return _properties;
     }
 
-    /**
-     * save all properties in property file <b>propertyFileName()</b>
-     */
     void properties::save() {
         // TODO # : bug/feature - it ignore all commentaries!
         std::ofstream fout(_property_filename);
@@ -138,10 +99,6 @@ namespace mc {
         fout.close();
     }
 
-    /**
-     * create empty property file with name <b>filename</b>
-     * @param filename
-     */
     void properties::create(const std::string& filename) {
         _property_filename = filename;
         std::ofstream fout(_property_filename);
@@ -149,13 +106,6 @@ namespace mc {
         fout.close();
     }
 
-    /**
-     * set a new property <b>key</b> with value <b>value</b>.
-     * return value is reserved.
-     * @param key
-     * @param value
-     * @return 
-     */
     bool properties::set_property(std::string key, std::string value) {
         _properties.insert(std::make_pair(key, value));
         return true;
--- properties.h
+++ properties.h
@@ -5,23 +5,76 @@
 
 namespace mc {
 
+    /**
+     * class for property files reading 
+     */
     class properties {
         std::map<std::string, std::string> _properties;
         std::string _property_filename;
     public:
+        /**
+         * default constructor. associated implicitly with file "default.properties"
+         */
         properties();
+        /**
+         * destructor
+         */
         virtual ~properties();
 
+        /**
+         * load properties from file <b>filename</b>
+         * @param filename
+         */
         void load_properties(const std::string&);
+        /**
+         * check if property exists. return false if property <b>key</b>
+         * does not exist.
+         * @param key
+         * @return 
+         */
         bool has_property(const std::string&) const;
+        /**
+         * return value of property <b>key</b>.
+         * is the same as <b>getProperty</b>.
+         * @param key
+         * @return 
+         */
         const std::string& operator[](const std::string&) const;
+        /**
+         * return value of property <b>key</b>.
+         * is the same as <b>operator []</b>.
+         * @param key
+         * @return 
+         */
         const std::string& get_property(const std::string&) const;
+        /**
+         * returns name of associated property file
+         * @return 
+         */
         const std::string& filename() const;
+        /**
+         * returns all properties as map
+         * @return 
+         */
         const std::map<std::string, std::string>& all_properties() const;
 
         // for adding new properties
+        /**
+         * save all properties in property file <b>propertyFileName()</b>
+         */
         void save();
+        /**
+         * create empty property file with name <b>filename</b>
+         * @param filename
+         */
         void create(const std::string&);
+        /**
+         * set a new property <b>key</b> with value <b>value</b>.
+         * return value is reserved.
+         * @param key
+         * @param value
+         * @return 
+         */
         bool set_property(std::string, std::string);
     private:
 
